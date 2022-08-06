66179124768a77e10d459d7797507b16f9b7cce6
Serhii Pimenov <serhii@pimenov.com.ua>
2022-08-06 10:13:30 +0300

    ...

--- src/graphql/resolvers/components/health.js
+++ src/graphql/resolvers/components/health.js
@@ -4,10 +4,12 @@ export const health = async (_, args) => {
     const nodes = !args.node && !args.nodes ?
         [config.aptos.api] :
         args.node ? [args.node] : [...args.nodes]
+
     const result = []
     for(let node of nodes) {
         const aptos = new Aptos(node)
         const response = await aptos.getHealthy()
+        console.log(response)
         result.push({
             node,
             status: response.ok ? response.payload : response.message
